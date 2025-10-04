d16a1692cd68744e54925a40b0632d47e9dfb632
m-rokon <ratertara.rokon@gmail.com>
2025-10-04 20:19:22 +0600

    nested componenet

--- src/index.js
+++ src/index.js
@@ -2,8 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function Greetings() {
-	return <h2>Hello from React</h2>;
+	return (
+		<div>
+			<Person />
+			<Message />
+		</div>
+	);
 }
+
+const Person = () => <h2>Name is Rokon</h2>;
+const Message = () => {
+	return <p>This example is showing the nested component.</p>;
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
