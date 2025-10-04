5bd08ca686280837221deffcfce9182cad2661ed
m-rokon <ratertara.rokon@gmail.com>
2025-10-04 17:47:44 +0600

    first component render into the app

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 function Greetings() {
 	return <h2>Hello from React</h2>;
 }
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Greetings />);
