02c6a87dbfcf07f66f0fc7dd5b8328d0450104e0
m-rokon <ratertara.rokon@gmail.com>
2025-11-24 23:46:08 +0600

    array example set

--- 03-advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,14 @@
+import { useState } from "react";
+import { data } from "../../../data";
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+	const [people, setPeople] = useState(data);
+	return (
+		<div>
+			{people.map(({ id, name }) => {
+				return <h2>{name}</h2>;
+			})}
+		</div>
+	);
 };
 
 export default UseStateArray;
