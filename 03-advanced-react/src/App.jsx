358ec592768d1d11a1f234f2e5912182d46c1467
m-rokon <ratertara.rokon@gmail.com>
2025-11-24 23:35:31 +0600

    array example init

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,11 +1,13 @@
 // import ErrorExample from "./tutorial/01-useState/starter/01-error-example";
 import UseStateBasics from "./tutorial/01-useState/starter/02-useState-basics";
+import UseStateArray from "./tutorial/01-useState/starter/03-useState-array";
 
 function App() {
 	return (
 		<div className="container">
 			{/* <ErrorExample /> */}
-			<UseStateBasics />
+			{/* <UseStateBasics /> */}
+			<UseStateArray />
 		</div>
 	);
 }
