6534cc17ad1a723d066301e799464519fa0eeda0
Mohammad Al Mamun Rokon <rokon@Rokons-MBP.local>
2025-11-30 10:16:02 +0600

    `useState` object example init

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,15 +1,17 @@
 // import ErrorExample from "./tutorial/01-useState/starter/01-error-example";
-import UseStateBasics from "./tutorial/01-useState/starter/02-useState-basics";
-import UseStateArray from "./tutorial/01-useState/starter/03-useState-array";
+// import UseStateBasics from "./tutorial/01-useState/starter/02-useState-basics";
+// import UseStateArray from "./tutorial/01-useState/starter/03-useState-array";
+import UseStateObject from "./tutorial/01-useState/starter/04-useState-object";
 
 function App() {
-	return (
-		<div className="container">
-			{/* <ErrorExample /> */}
-			{/* <UseStateBasics /> */}
-			<UseStateArray />
-		</div>
-	);
+    return (
+        <div className="container">
+            {/* <ErrorExample /> */}
+            {/* <UseStateBasics /> */}
+            {/* <UseStateArray /> */}
+            <UseStateObject />
+        </div>
+    );
 }
 
 export default App;
