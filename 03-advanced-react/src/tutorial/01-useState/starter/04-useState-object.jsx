f35f241b92f6027acdc60bf034f4e14238f0d188
Mohammad Al Mamun Rokon <rokon@Rokons-MBP.local>
2025-11-30 11:57:14 +0600

    `useState` object example complete

--- 03-advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,23 +1,30 @@
 import { useState } from "react";
 
 const UseStateObject = () => {
-    const [name, setName] = useState("Rokon");
-    const [age, setAge] = useState(24);
-    const [hobby, setHobby] = useState("Reading books");
+    // const [name, setName] = useState("Rokon");
+    // const [age, setAge] = useState(24);
+    // const [hobby, setHobby] = useState("Reading books");
+
+    const [person, setPerson] = useState({
+        name: "Rahim",
+        age: 25,
+        hobby: "Gardening",
+    });
 
     const displayPerson = () => {
-        setName("Raj");
-        setAge(27);
-        setHobby("Swim");
+        // setName("Raj");
+        // setAge(27);
+        // setHobby("Swim");
+        setPerson({ name: "Rafi", age: 26, hobby: "Running" });
     };
 
     return (
         <>
-            <h2>{name}</h2>
-            <h3>{age}</h3>
-            <h4>Enjoys: {hobby}</h4>
+            <h2>{person.name}</h2>
+            <h3>{person.age}</h3>
+            <h4>Enjoys: {person.hobby}</h4>
             <button className="btn" onClick={displayPerson}>
-                Show Raj
+                Show Person
             </button>
         </>
     );
