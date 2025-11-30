ea8376330df1dced5e472ee66e6f7d6f11a700dd
Mohammad Al Mamun Rokon <rokon@Rokons-MBP.local>
2025-11-30 11:50:37 +0600

    `useState` object example with just useState - changing details

--- 03-advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -5,11 +5,20 @@ const UseStateObject = () => {
     const [age, setAge] = useState(24);
     const [hobby, setHobby] = useState("Reading books");
 
+    const displayPerson = () => {
+        setName("Raj");
+        setAge(27);
+        setHobby("Swim");
+    };
+
     return (
         <>
             <h2>{name}</h2>
             <h3>{age}</h3>
             <h4>Enjoys: {hobby}</h4>
+            <button className="btn" onClick={displayPerson}>
+                Show Raj
+            </button>
         </>
     );
 };
