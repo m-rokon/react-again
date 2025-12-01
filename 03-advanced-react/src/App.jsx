a444668c4f20c910616571a838383fa1c6235184
m-rokon <ratertara.rokon@gmail.com>
2025-12-02 01:00:18 +0600

    - gotcha init

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,17 +1,19 @@
 // import ErrorExample from "./tutorial/01-useState/starter/01-error-example";
 // import UseStateBasics from "./tutorial/01-useState/starter/02-useState-basics";
 // import UseStateArray from "./tutorial/01-useState/starter/03-useState-array";
-import UseStateObject from "./tutorial/01-useState/starter/04-useState-object";
+// import UseStateObject from "./tutorial/01-useState/starter/04-useState-object";
+import UseStateGotcha from "./tutorial/01-useState/starter/05-useState-gotcha";
 
 function App() {
-    return (
-        <div className="container">
-            {/* <ErrorExample /> */}
-            {/* <UseStateBasics /> */}
-            {/* <UseStateArray /> */}
-            <UseStateObject />
-        </div>
-    );
+	return (
+		<div className="container">
+			{/* <ErrorExample /> */}
+			{/* <UseStateBasics /> */}
+			{/* <UseStateArray /> */}
+			{/* <UseStateObject /> */}
+			<UseStateGotcha />
+		</div>
+	);
 }
 
 export default App;
