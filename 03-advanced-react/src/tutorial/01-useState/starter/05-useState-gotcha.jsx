64bb247810b2ade58aeb1538d76e99e7d8e14b80
m-rokon <ratertara.rokon@gmail.com>
2025-12-02 01:19:38 +0600

    - gotcha with fix solution

--- 03-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -4,8 +4,15 @@ const UseStateGotcha = () => {
 	const [value, setValue] = useState(0);
 
 	const handleClick = () => {
-		setValue(value + 1);
-		console.log(value);
+		// setValue(value + 1);
+		// console.log(value);
+		/* for the above setup, console will always be 1 step behind.. to solve this, we can use the following process */
+		setValue((currentState) => {
+			const newValue = currentState + 1;
+			console.log(newValue);
+
+			return newValue;
+		});
 	};
 
 	return (
