68794c28e8ca0d695ef05fa1d0d154056d0db8a2
m-rokon <ratertara.rokon@gmail.com>
2025-12-02 01:04:03 +0600

    - gotcha setup

--- 03-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,21 @@
+import { useState } from "react";
+
 const UseStateGotcha = () => {
-	return <h2>useState "gotcha"</h2>;
+	const [value, setValue] = useState(0);
+
+	const handleClick = () => {
+		setValue(value + 1);
+		console.log(value);
+	};
+
+	return (
+		<div>
+			<h1>{value}</h1>
+			<button type="button" className="btn" onClick={handleClick}>
+				increase
+			</button>
+		</div>
+	);
 };
 
 export default UseStateGotcha;
