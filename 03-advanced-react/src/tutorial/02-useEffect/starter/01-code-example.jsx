41d14270f8056394d3b2d35c703a950142c22f51
m-rokon <ratertara.rokon@gmail.com>
2025-12-06 02:00:06 +0600

    `UseEffect` - basic usage

--- 03-advanced-react/src/tutorial/02-useEffect/starter/01-code-example.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/01-code-example.jsx
@@ -1,15 +1,24 @@
-import { useState } from 'react';
+import { useEffect, useState } from "react";
 
 const CodeExample = () => {
-  const [value, setValue] = useState(0);
+	const [value, setValue] = useState(0);
 
-  return (
-    <div>
-      <h1>value : {value}</h1>
-      <button className='btn' onClick={() => setValue(value + 1)}>
-        click me
-      </button>
-    </div>
-  );
+	const sayHello = () => {
+		console.log("hello there!");
+	};
+	sayHello();
+
+	useEffect(() => {
+		console.log("Hello from useEffect");
+	}, []);
+
+	return (
+		<div>
+			<h1>value : {value}</h1>
+			<button className="btn" onClick={() => setValue(value + 1)}>
+				click me
+			</button>
+		</div>
+	);
 };
 export default CodeExample;
