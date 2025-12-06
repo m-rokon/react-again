215b6055fc6048967c51ca8797b6a33546004ce7
m-rokon <ratertara.rokon@gmail.com>
2025-12-07 00:53:14 +0600

    `multiple returns` Fetchdata responses

--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -1,7 +1,26 @@
-import { useEffect, useState } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import { useEffect, useState } from "react";
+const url = "https://api.github.com/users/QuincyLarson";
 
 const MultipleReturnsFetchData = () => {
-  return <h2>Fetch Data </h2>;
+	const [user, setUser] = useState(null);
+
+	useEffect(() => {
+		try {
+			const fetchData = async () => {
+				const response = await fetch(url);
+				const user = await response.json();
+				setUser(user);
+				console.log(user);
+			};
+			fetchData();
+		} catch (error) {
+			console.log(error);
+		}
+	}, []);
+	return (
+		<div>
+			<h2>Fetch user</h2>
+		</div>
+	);
 };
 export default MultipleReturnsFetchData;
