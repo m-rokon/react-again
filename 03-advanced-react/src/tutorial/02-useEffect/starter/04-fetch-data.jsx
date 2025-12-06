386e91cb23487f138a570fffb254bddacae9b856
m-rokon <ratertara.rokon@gmail.com>
2025-12-06 22:33:36 +0600

    `fetch API` challenge completed

--- 03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,41 @@
-const url = 'https://api.github.com/users';
+import { useEffect, useState } from "react";
+
+const url = "https://api.github.com/users";
 
 const FetchData = () => {
-  return <h2>fetch data example</h2>;
+	const [users, setUsers] = useState([]);
+
+	useEffect(() => {
+		const data = async () => {
+			try {
+				const result = await fetch(url);
+				const users = await result.json();
+				console.log(users);
+				setUsers(users);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		data();
+	}, []);
+	return (
+		<section>
+			<h3>github users</h3>
+			<ul className="users">
+				{users.map((user) => {
+					const { id, avatar_url, html_url, login } = user;
+					return (
+						<li key={id}>
+							<img src={avatar_url} alt={login} />
+							<div>
+								<h5>{login}</h5>
+								<a href={html_url}>profile</a>
+							</div>
+						</li>
+					);
+				})}
+			</ul>
+		</section>
+	);
 };
 export default FetchData;
