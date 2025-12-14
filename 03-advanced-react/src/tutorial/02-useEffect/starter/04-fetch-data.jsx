f3496906d24faf1304a96fccc1cb29f29b1dd737
Mohammad Al Mamun Rokon <rokon@Rokons-MBP.local>
2025-12-15 00:45:06 +0600

    `fetch` data gotcha and solution implemented

--- 03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -3,39 +3,39 @@ import { useEffect, useState } from "react";
 const url = "https://api.github.com/users";
 
 const FetchData = () => {
-	const [users, setUsers] = useState([]);
+    const [users, setUsers] = useState([]);
 
-	useEffect(() => {
-		const data = async () => {
-			try {
-				const result = await fetch(url);
-				const users = await result.json();
-				console.log(users);
-				setUsers(users);
-			} catch (error) {
-				console.log(error);
-			}
-		};
-		data();
-	}, []);
-	return (
-		<section>
-			<h3>github users</h3>
-			<ul className="users">
-				{users.map((user) => {
-					const { id, avatar_url, html_url, login } = user;
-					return (
-						<li key={id}>
-							<img src={avatar_url} alt={login} />
-							<div>
-								<h5>{login}</h5>
-								<a href={html_url}>profile</a>
-							</div>
-						</li>
-					);
-				})}
-			</ul>
-		</section>
-	);
+    useEffect(() => {
+        const data = async () => {
+            try {
+                const result = await fetch(url);
+                const users = await result.json();
+                console.log(users);
+                setUsers(users);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        data();
+    }, []);
+    return (
+        <section>
+            <h3>github users</h3>
+            <ul className="users">
+                {users.map((user) => {
+                    const { id, avatar_url, html_url, login } = user;
+                    return (
+                        <li key={id}>
+                            <img src={avatar_url} alt={login} />
+                            <div>
+                                <h5>{login}</h5>
+                                <a href={html_url}>profile</a>
+                            </div>
+                        </li>
+                    );
+                })}
+            </ul>
+        </section>
+    );
 };
 export default FetchData;
--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -2,45 +2,49 @@ import { useEffect, useState } from "react";
 const url = "https://api.github.com/users/QuincyLarson";
 
 const MultipleReturnsFetchData = () => {
-	const [isError, setIsError] = useState(false);
-	const [isLoading, setIsLoading] = useState(true);
-	const [user, setUser] = useState(null);
+    const [isError, setIsError] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+    const [user, setUser] = useState(null);
 
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const response = await fetch(url);
-				const user = await response.json();
-				setUser(user);
-				// console.log(user);
-			} catch (error) {
-				setIsError(true);
-				console.log(error);
-			}
-			setIsLoading(false);
-		};
-		fetchData();
-	}, []);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(url);
+                if (!response.ok) {
+                    setIsError(true);
+                    setIsLoading(false);
+                }
+                const user = await response.json();
+                setUser(user);
+                // console.log(user);
+            } catch (error) {
+                setIsError(true);
+                console.log(error);
+            }
+            setIsLoading(false);
+        };
+        fetchData();
+    }, []);
 
-	if (isLoading) {
-		return <h2>Loading...</h2>;
-	}
+    if (isLoading) {
+        return <h2>Loading...</h2>;
+    }
 
-	if (isError) {
-		return <h2>There was an error...</h2>;
-	}
+    if (isError) {
+        return <h2>There was an error...</h2>;
+    }
 
-	return (
-		<div>
-			<img
-				style={{ width: "150px", borderRadius: "15px" }}
-				src={user.avatar_url}
-				alt={user.login}
-			/>
-			<h2>{user.name}</h2>
-			<h5>Works at: {user.company}</h5>
-			<p>{user.bio}</p>
-		</div>
-	);
+    return (
+        <div>
+            <img
+                style={{ width: "150px", borderRadius: "15px" }}
+                src={user.avatar_url}
+                alt={user.login}
+            />
+            <h2>{user.name}</h2>
+            <h5>Works at: {user.company}</h5>
+            <p>{user.bio}</p>
+        </div>
+    );
 };
 export default MultipleReturnsFetchData;
