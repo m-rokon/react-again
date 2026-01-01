cfc3cda52c22d5406cf30371ae7021453e70dc8f
m-rokon <ratertara.rokon@gmail.com>
2026-01-01 16:28:26 +0600

    `short-circut` overview complete

--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
@@ -1,6 +1,17 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const ShortCircuitOverview = () => {
-  return <h2>short circuit overview</h2>;
+	const [text, setText] = useState("");
+	const [name, setName] = useState("Rokon");
+
+	return (
+		// if condtion will not work
+		<div>
+			<h4>Falsy OR: {text || "Hello World"}</h4>
+			<h4>Falsy AND: {text && "Hello World"}</h4>
+			<h4>Truthy OR: {name || "Hello world"}</h4>
+			<h4>Truthy AND: {name && "Hello World"}</h4>
+		</div>
+	);
 };
 export default ShortCircuitOverview;
