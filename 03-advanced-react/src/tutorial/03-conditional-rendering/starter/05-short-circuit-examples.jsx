77ce6497b20bbdc8177064ac04c26cb2cda072bf
m-rokon <ratertara.rokon@gmail.com>
2026-01-01 21:31:09 +0600

    `short-circut` - loading component conditionally

--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
@@ -8,6 +8,15 @@ const ShortCircuitExamples = () => {
 	const [user, setUser] = useState({ name: "john" });
 	const [isEditing, setIsEditing] = useState(false);
 
+	const SomeComponent = ({ name }) => {
+		return (
+			<div>
+				<h2>Welcome, {name}</h2>
+				<button className="btn">Logout</button>
+			</div>
+		);
+	};
+
 	return (
 		<div>
 			<h2>{text || "default value"}</h2>
@@ -24,6 +33,7 @@ const ShortCircuitExamples = () => {
 					<h4>{name}</h4>
 				</div>
 			)}
+			{user && <SomeComponent name={name} />}
 		</div>
 	);
 };
