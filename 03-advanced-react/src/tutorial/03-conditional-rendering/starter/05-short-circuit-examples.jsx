d78275f9e529ec3e6f51e36afcb8f7d2532705e1
m-rokon <ratertara.rokon@gmail.com>
2026-01-01 19:10:21 +0600

    `short-circut` example setup

--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const ShortCircuitExamples = () => {
-  // falsy
-  const [text, setText] = useState('');
-  // truthy
-  const [name, setName] = useState('susan');
-  const [user, setUser] = useState({ name: 'john' });
-  const [isEditing, setIsEditing] = useState(false);
+	// falsy
+	const [text, setText] = useState("");
+	// truthy
+	const [name, setName] = useState("susan");
+	const [user, setUser] = useState({ name: "john" });
+	const [isEditing, setIsEditing] = useState(false);
 
-  return <h2>short circuit - examples</h2>;
+	return (
+		<div>
+			<h2>{text || "default value"}</h2>
+		</div>
+	);
 };
 
 export default ShortCircuitExamples;
