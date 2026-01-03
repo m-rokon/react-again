d7f2928f2ce0e4f6a195280b61874b33aa754231
m-rokon <ratertara.rokon@gmail.com>
2026-01-03 18:22:09 +0600

    `user challenge` complete

--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,5 +1,36 @@
+import { useState } from "react";
+
 const UserChallenge = () => {
-  return <h2>user challenge</h2>;
+	const [user, setUser] = useState(null);
+
+	const handleLogin = () => {
+		setUser({ name: "Rokon" });
+	};
+
+	const handleLogout = () => {
+		setUser(null);
+	};
+
+	return (
+		<div>
+			<h2 style={{ marginBottom: "1rem" }}>user challenge</h2>
+			{user ? (
+				<div>
+					<h4>Hello there, {user.name}</h4>
+					<button className="btn" onClick={handleLogout}>
+						logout
+					</button>
+				</div>
+			) : (
+				<div>
+					<h4>Please login</h4>
+					<button className="btn" onClick={handleLogin}>
+						login
+					</button>
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default UserChallenge;
