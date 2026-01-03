4e55b87175ca4bb7a0e5f7cc836f7596b6108fa8
m-rokon <ratertara.rokon@gmail.com>
2026-01-03 22:24:46 +0600

    `useEffect` - setup

--- 03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,11 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const CleanupFunction = () => {
-	const [toggle, seToggle] = useState(false);
+	const [toggle, setToggle] = useState(false);
+
+	const SimpleComponent = () => {
+		useEffect(() => {
+			console.log("Hi from simple component");
+		}, []);
+		return <h2>Hello</h2>;
+	};
 
 	return (
 		<div>
-			<button className="btn">toggle</button>
+			<button className="btn" onClick={() => setToggle(!toggle)}>
+				toggle
+			</button>
+			{toggle && <SimpleComponent />}
 		</div>
 	);
 };
